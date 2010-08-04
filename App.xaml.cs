a59209981aa76605ddde2547eabbd8e374f60d8d
Petr Onderka <gsvick@gmail.com>
2010-08-04 03:14:54 +0200

    code cleanup

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,26 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
-using System.Windows.Navigation;
 using System.Diagnostics;
+using System.Linq;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
-using System.Collections;
+using System.Windows.Markup;
+using System.Windows.Navigation;
+using System.Windows.Threading;
 using Newest_unaswered_by_tags.Properties;
-using Stacky;
 
 namespace Newest_unaswered_by_tags
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
 		public App()
 		{
-			FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(System.Windows.Markup.XmlLanguage.GetLanguage(System.Globalization.CultureInfo.CurrentCulture.IetfLanguageTag)));
+			FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
 		}
 
 		void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
@@ -28,7 +24,7 @@ namespace Newest_unaswered_by_tags
 			Process.Start(e.Uri.ToString());
 		}
 
-		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
 		{
 			Exception ex = e.Exception;
 			((MainWindow)MainWindow).ShowException(ex);
@@ -64,5 +60,4 @@ namespace Newest_unaswered_by_tags
 			return null;
 		}
 	}
-
 }
