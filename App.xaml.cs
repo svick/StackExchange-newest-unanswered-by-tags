ed9e289ab4e52a0089b77692ad25c9fdfd0b5dbc
Petr Onderka <gsvick@gmail.com>
2010-08-04 03:35:31 +0200

    mv ValueConverters to their own file

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Globalization;
 using System.Windows;
-using System.Windows.Data;
 using System.Windows.Markup;
 using System.Windows.Navigation;
 using System.Windows.Threading;
-using Newest_unaswered_by_tags.Properties;
 
 namespace Newest_unaswered_by_tags
 {
@@ -28,36 +24,6 @@ namespace Newest_unaswered_by_tags
 		{
 			Exception ex = e.Exception;
 			((MainWindow)MainWindow).ShowException(ex);
-		}
-	}
-
-	[ValueConversion(typeof(long), typeof(Uri))]
-	public class IdToUrlConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			int id = (int)value;
-
-			return new Uri(StackySite.GetSite(Settings.Default.Site).SiteUrl + "/questions/" + id.ToString());
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			return null;
-		}
-	}
-
-	[ValueConversion(typeof(IEnumerable<object>), typeof(string))]
-	public class EnumerableToStringConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			return string.Join(" ", ((IEnumerable<object>)value).Select(o => o.ToString()));
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			return null;
 		}
 	}
 }
---
+++ ValueConverters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Data;
+using Newest_unaswered_by_tags.Properties;
+
+namespace Newest_unaswered_by_tags
+{
+	[ValueConversion(typeof(long), typeof(Uri))]
+	public class IdToUrlConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			int id = (int)value;
+
+			return new Uri(StackySite.GetSite(Settings.Default.Site).SiteUrl + "/questions/" + id.ToString());
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return null;
+		}
+	}
+
+	[ValueConversion(typeof(IEnumerable<object>), typeof(string))]
+	public class EnumerableToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return string.Join(" ", ((IEnumerable<object>)value).Select(o => o.ToString()));
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
